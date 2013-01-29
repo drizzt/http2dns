670f2c5c4933bb43b50891833bf25971c2e67817
Timothy Redaelli <timothy.redaelli@gmail.com>
2013-01-29 18:57:39 +0100

    Use msgpack instead of json

--- web.rb
+++ web.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
-require 'json'
+require 'msgpack'
+require 'resolv'
 
 get '/:host' do |host|
 	dns = Resolv::DNS.new(nameserver: %w(8.8.8.8 8.8.4.4))
 	[Resolv::DNS::Resource::IN::AAAA, Resolv::DNS::Resource::IN::A].flat_map do |klass|
-		dns.getresources(host, klass).collect {|r| [r.ttl, r.address.to_s]}
-	end.to_json
+		dns.getresources(host, klass).collect {|r| [r.ttl, r.address.address]}
+	end.to_msgpack
 end
