c49b14ce04483977681115d106e008e4407cb8e0
Timothy Redaelli <timothy.redaelli@gmail.com>
2013-01-29 15:15:55 +0100

    ttl should be before address

--- web.rb
+++ web.rb
@@ -4,6 +4,6 @@ require 'json'
 get '/:host' do |host|
 	dns = Resolv::DNS.new(nameserver: %w(8.8.8.8 8.8.4.4))
 	[Resolv::DNS::Resource::IN::AAAA, Resolv::DNS::Resource::IN::A].flat_map do |klass|
-		dns.getresources(host, klass).collect {|r| [r.address.to_s, r.ttl]}
+		dns.getresources(host, klass).collect {|r| [r.ttl, r.address.to_s]}
 	end.to_json
 end
